9d3be143c9f2986fbc1695f97da652d73b486f23
DESKTOP-5K7FJ6E\thunder <1652575963@qq.com>
2018-10-31 18:25:22 +0800

    课程列表

---
+++ js/course-detail-video.js
@@ -0,0 +1,93 @@
+window.onload = function () {
+    let app = new Vue({
+        el: '#app',
+        data: {
+            isHidden:true,
+            openTitle:'展开全部',
+            courseList:[
+                {
+                    id:0,
+                    title:'广场舞16步',
+                    desc:'适合初学者，一般由4个4步组成',
+                    isOpen:true
+                },
+                {
+                    id:1,
+                    title:'广场舞16步',
+                    desc:'适合初学者，一般由4个4步组成',
+                    isOpen:true
+                },
+                {
+                    id:2,
+                    title:'广场舞16步',
+                    desc:'适合初学者，一般由4个4步组成',
+                    isOpen:false
+                },
+                {
+                    id:3,
+                    title:'广场舞16步',
+                    desc:'适合初学者，一般由4个4步组成',
+                    isOpen:true
+                },
+                {
+                    id:4,
+                    title:'广场舞16步',
+                    desc:'适合初学者，一般由4个4步组成',
+                    isOpen:false
+                }
+            ],
+            //评论输入的内容
+            commentsContent:'',
+            //评论发表聚焦
+            isFocus:false,
+            //发送按钮禁用
+            isDisabled:true
+        },
+        methods: {
+            //展开列表
+            openAllCourse:function () {
+                if(this.isHidden) {
+                    this.openTitle = '收起列表';
+                }
+                else {
+                    this.openTitle = '展开全部';
+                }
+                this.isHidden = !this.isHidden;
+            },
+            //显示上传图片图标等
+            hideSmile:function () {
+                this.isFocus = true;
+            },
+            //隐藏上传图标
+            showSmile:function () {
+                this.isFocus = false;
+            },
+            //上传图片
+            uploadImg:function () {
+                document.getElementById('upload-img').click();
+            }
+        },
+        watch: {
+            commentsContent:function (newVal,oldVal) {
+                if(newVal.trim() != '') {
+                    this.isDisabled = false;
+                }
+                else {
+                    this.isDisabled = true;
+                }
+            }
+        }
+    });
+    /**
+     * 监听文件上传框变化
+     */
+    $("#upload-img").change(function () {
+        let reads= new FileReader();
+        let f=document.getElementById('upload-img').files[0];
+        reads.readAsDataURL(f);
+        reads.onload=function (e) {
+            document.getElementById('show').src=this.result;
+            mui.toast('文件读取成功!');
+        };
+    });
+};
---
+++ js/course-hour-detail.js
@@ -0,0 +1,91 @@
+window.onload = function () {
+    //vue实例化
+    let app = new Vue({
+        el: '#app',
+        data: {
+            isPlay: false,
+            videoTotalTime: '00:00',
+            currentTime: '00:00',
+            videoPlayer: ''
+        },
+        methods: {
+            startPlay: function () {
+                this.videoPlayer.play();
+                this.isPlay = true;
+                setTimeout(function () {
+                    $(".btn-group-play").hide(200);
+                    $(".video-progress-bar-container").hide(200);
+                }, 3000);
+            },
+            pausePlay: function () {
+                this.videoPlayer.pause();
+                this.isPlay = false;
+            },
+            //播放器点击显示按钮组
+            showBtnGroup: function () {
+                $(".btn-group-play").show();
+                $(".video-progress-bar-container").show();
+                setTimeout(function () {
+                    $(".btn-group-play").hide(200);
+                    $(".video-progress-bar-container").hide(200);
+                }, 3000);
+
+            },
+            //全屏
+            fullScreen: function () {
+                requestFullScreen();
+            }
+        },
+        created: function () {
+            let vm = this;
+            //video实例
+            document.getElementById('video-player').addEventListener('loadedmetadata', function () {
+                //视频加载完成
+                vm.videoPlayer = document.getElementById('video-player');
+                vm.videoTotalTime = transformTime(this.duration);
+                vm.currentTime = transformTime(this.currentTime);
+            });
+
+            let interval = setInterval(function () {
+                if (vm.videoPlayer.currentTime == vm.videoPlayer.duration) {
+                    vm.isPlay = false;
+                    //播放完毕
+                    $(".btn-group-play").show();
+                    $(".video-progress-bar-container").show();
+                }
+                vm.currentTime = transformTime(vm.videoPlayer.currentTime);
+                mui("#video-progress-bar").progressbar({progress: vm.videoPlayer.currentTime / vm.videoPlayer.duration * 100}).show();
+            }, 1000);
+        }
+    });
+}
+
+function transformTime(ms) {
+    let minute = parseInt(ms / 60);
+    let seconds = parseInt((ms / 60 - minute) * 60);
+    minute = minute < 10 ? `0${minute}` : minute;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minute} : ${seconds}`;
+}
+
+function requestFullScreen() {
+    var de = document.documentElement;
+    if (de.requestFullscreen) {
+        de.requestFullscreen();
+    } else if (de.mozRequestFullScreen) {
+        de.mozRequestFullScreen();
+    } else if (de.webkitRequestFullScreen) {
+        de.webkitRequestFullScreen();
+    }
+}
+
+function exitFullscreen() {
+    var de = document;
+    if (de.exitFullscreen) {
+        de.exitFullscreen();
+    } else if (de.mozCancelFullScreen) {
+        de.mozCancelFullScreen();
+    } else if (de.webkitCancelFullScreen) {
+        de.webkitCancelFullScreen();
+    }
+}
